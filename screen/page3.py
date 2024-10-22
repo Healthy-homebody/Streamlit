0a89502d6abcbc54bf66c2dbc21491dc612fb603
seohyeon <uimi3606@gmail.com>
2024-10-22 22:58:06 +0900

    🐛 DTWEX 연결

--- screen/page3.py
+++ screen/page3.py
@@ -1,39 +1,14 @@
 # 동작 비교 페이지 (page3)
 import streamlit as st
+import sys
 import os
 import cv2
 import tempfile  # 임시 파일을 저장하기 위해 사용
-# from yolo import process_frame
-# from compare_videos import compare_videos  # 동영상 비교를 위한 함수 가져오기
+from ultralytics import YOLO
+from models.DTWEX import compare_videos 
 
-# pip install yolov8
-
-def process_video_to_file(video_path, output_path):
-    """YOLO로 처리한 비디오를 파일로 저장."""
-    cap = cv2.VideoCapture(video_path)
-    
-    # # 비디오 속성 가져오기
-    # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    # fps = cap.get(cv2.CAP_PROP_FPS)
-    # width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    # height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    
-    # # YOLO 처리된 비디오 저장을 위한 VideoWriter 객체 생성
-    # out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
-
-    # while cap.isOpened():
-    #     ret, frame = cap.read()
-    #     if not ret:
-    #         break
-
-    #     # YOLO 모델로 프레임 처리
-    #     frame = process_frame(frame)
-
-    #     # YOLO로 처리된 프레임을 저장
-    #     out.write(frame)
-
-    # cap.release()
-    # out.release()
+# 시스템 경로 추가
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def save_uploaded_file(uploaded_file):
     """업로드된 비디오 파일을 임시 파일로 저장하고, 그 파일 경로를 반환."""
@@ -44,6 +19,9 @@ def save_uploaded_file(uploaded_file):
 def show():
     st.title("동작 비교 페이지")
     st.write("여기는 동작 비교 페이지입니다.")
+
+    # YOLO 모델 로드
+    model = YOLO('yolov8n-pose.pt')
 
     # 동작 설명 비디오 처리
     if 'selected_action' in st.session_state:
@@ -61,6 +39,7 @@ def show():
             description_video_path = video_path  # 설명 비디오 경로 저장
         else:
             st.write("비디오 파일을 찾을 수 없습니다.")
+            description_video_path = None  # 비디오 경로가 없으면 None으로 설정
     else:
         st.subheader("동작 설명 비디오")
         st.write("비디오가 없습니다.")
@@ -82,7 +61,7 @@ def show():
             # 동작 유사도 측정 중이라는 메시지 표시
             with st.spinner('동작 유사도 측정 중...'):
                 # 유사도 측정을 위해 DTW 모델 적용
-                compare_videos(description_video_path, uploaded_video_path, model=None)  # compare_videos 함수 실행
+                compare_videos(description_video_path, uploaded_video_path, model=model)  # compare_videos 함수 실행
             
             st.success('유사도 측정 완료!')
     else:
